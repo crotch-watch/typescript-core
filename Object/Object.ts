1962d6dd47b85d67e05769e9a7d23ea83c41ab0a
crotch-watch <naps_08_bay@icloud.com>
2025-04-14 22:54:22 +0530

    add: typing obj. and aliases

---
+++ Object/Object.ts
@@ -0,0 +1,23 @@
+let obj = { id: 1, name: "John" }
+obj.job = "developer"
+
+let details: Object = { name: "John", email: "john@example.com" }
+details = [details]
+details = async () => details
+
+let car: {} = {
+  brand: "Honda",
+  model: "Civic"
+}
+car.wheels = 4
+
+car = function () {}
+car = new Set([car])
+
+let home: {
+  rooms: number
+  area: number
+} = {
+  rooms: 5,
+  area: 1100
+}
