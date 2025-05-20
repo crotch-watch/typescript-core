5decf60fd235107f25ca04a1336c96e2f5b68095
crotch-watch <naps_08_bay@icloud.com>
2025-05-20 22:54:56 +0530

    add: accessors and mutators

--- Classes/AccessorsMutators.ts
+++ Classes/AccessorsMutators.ts
@@ -4,6 +4,7 @@ class School {
   // either this validation can be present in the constructor
   // or setter can do it.
   private _rooms?: number
+  public _chambers?: number
 
   constructor(public name: string) {}
 
@@ -14,6 +15,22 @@ class School {
     }
     this._rooms = rooms
   }
+
+  // return type is already inferred based on set
+  // used with set
+  public get rooms () {
+    if(this._rooms) return this._rooms
+    throw new Error('property rooms has not been set.')
+  }
+
+  // accessors can be separately used without mutators
+  public get chambers() {
+    return this._chambers
+  }
 }
 
 const school = new School('mySchool')
+school.rooms = 12
+console.log(school.rooms)
+school.chambers
+
